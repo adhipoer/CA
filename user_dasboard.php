6fbd64a83552b4a1b014341095d13c026ae6dc51
frhdina <farahdina@outlook.com>
2015-05-06 22:02:29 +0700

    user dashboard

--- user_dasboard.php
+++ user_dasboard.php
@@ -33,13 +33,13 @@
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Link</a></li>
+            <li class="active"><a href="create-csr.php">CSR <span class="sr-only"></span></a></li>
+			<li><a href="#">CA SIGN</a></li>
+			<li><a href="create-x509-CAsigned.php">Self Sign</a></li>
+			<li><a href="#">Revoke</a></li>
         </ul>
         <ul class="nav navbar-nav navbar-right">
-            <li> Hello, <?php echo htmlentities($_SESSION['email']['user_organization'], ENT_QUOTES, 'UTF-8'); ?></li>
-        </ul>
-        <ul class="nav navbar-nav navbar-right">
+		    <li><a> Hello, <?php echo htmlentities($_SESSION['email']['user_organization'], ENT_QUOTES, 'UTF-8'); ?></a></li>
             <li><a href="logout.php">Logout</a></li>
         </ul>
     </div>
